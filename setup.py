d1f0abc56a7f9a03c51e837d6c23593dd8f57065
Rohit Sharma <fakeeclipx@gmail.com>
2025-11-06 20:33:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='GenerativeAi_encyclopedia',
+    version='0.1.0',
+    author='Eclipse',
+    author_email="randomrohit002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
